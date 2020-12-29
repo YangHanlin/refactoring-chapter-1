96fbf69becb61af5e1de40c8d2b3bea1c23058db
Yang Hanlin <mattoncis@hotmail.com>
2020-12-30 02:58:38 +0800

    feat: 1.3: p29

--- src/main/java/com/movie/rental/Customer.java
+++ src/main/java/com/movie/rental/Customer.java
@@ -20,12 +20,11 @@ public class Customer {
     }
 
     public String statement() {
-        int frequentRenterPoints = 0;
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
-            frequentRenterPoints += each.getFrequentRenterPoints();
+
 
             // show figures for this rental
             result += "\t" + each.getMovie().getTitle() + "\t" +
@@ -33,7 +32,7 @@ public class Customer {
         }
         // aed footer lines
         result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) +
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
                 " frequent renter points";
         return result;
     }
@@ -44,6 +43,16 @@ public class Customer {
         while (rentals.hasMoreElements()) {
             Rental each = (Rental) rentals.nextElement();
             result += each.getCharge();
+        }
+        return result;
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        int result = 0;
+        Enumeration rentals = _rentals.elements();
+        while (rentals.hasMoreElements()) {
+            Rental each = (Rental) rentals.nextElement();
+            result += each.getFrequentRenterPoints();
         }
         return result;
     }
