9a8f33d4ee26b022f133694ce8a93c6285e76958
Yang Hanlin <mattoncis@hotmail.com>
2020-12-30 11:18:33 +0800

    feat: 1.4: p47
    This chapter starts to really convert switch to polymorphism

--- src/main/java/com/movie/rental/NewReleasePrice.java
+++ src/main/java/com/movie/rental/NewReleasePrice.java
@@ -5,4 +5,10 @@ public class NewReleasePrice extends Price {
     int getPriceCode() {
         return Movie.NEW_RELEASE;
     }
+
+    @Override
+    public double getCharge(int daysRented) {
+        double result = daysRented * 3;
+        return result;
+    }
 }
